883d1a2aca5987df4d5e253d18cea9fb01b27a52
Bobby Stuijfzand <32268468+BobbyGlennS@users.noreply.github.com>
2019-11-13 13:58:48 +0100

    Add exception to MD60 for numbered lists
    Exception makes sure numbered lists (e.g. 1. list item, 2. list item) don't get flagged up as a violation of rule MD60 (i.e. More than one sentence in line).
    
    This is particularly relevant now we have `enslide_list()`

--- lib/mdl/rules.rb
+++ lib/mdl/rules.rb
@@ -683,7 +683,7 @@ rule "MD060", "More than one sentence in line" do
   tags :whitespace, :emphasis
   aliases 'single-sentence'
   check do |doc|
-	  doc.matching_text_element_lines(/(?<!i\.e|e\.g)\. ./).sort
+	  doc.matching_text_element_lines(/(?<!i\.e|e\.g\d)\. ./).sort
   end
 end
 
